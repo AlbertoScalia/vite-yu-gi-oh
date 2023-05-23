a4b492dde307e74ff79cf8fb276b67a1f693d02e
AlbertoScalia <albscalia@gmail.com>
2023-05-23 14:55:21 +0200

    Feat: Add card structure

--- src/store.js
+++ src/store.js
@@ -1,5 +1,18 @@
 import { reactive } from 'vue'
+import axios from 'axios'
 
 export const store = reactive({
-  YUGI_API: 'https://db.ygoprodeck.com/api/v7/cardinfo.php?num=15&offset=0'
+  loading: true,
+  YUGI_API: 'https://db.ygoprodeck.com/api/v7/cardinfo.php?num=15&offset=0',
+  cards: null,
+  cardName: null,
+  cardArchetype: null,
+  fetchCard() {
+    axios.get(this.YUGI_API)
+    .then(response => {
+      this.cards = response.data
+      this.cardName = response.data.name
+      this.cardArchetype = response.data.archetype
+    })
+  }
 })
