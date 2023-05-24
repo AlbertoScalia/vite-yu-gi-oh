37d09d8a1f5f222fa608e1fdd3e17822fca58d9b
AlbertoScalia <albscalia@gmail.com>
2023-05-24 14:37:29 +0200

    Feat: Add select archetypes and catch error

--- src/store.js
+++ src/store.js
@@ -3,13 +3,17 @@ import axios from 'axios'
 
 export const store = reactive({
   loading: true,
-  YUGI_API: 'https://db.ygoprodeck.com/api/v7/cardinfo.php?num=15&offset=0',
+  YUGI_API: 'https://db.ygoprodeck.com/api/v7/cardinfo.php?num=30&offset=0',
   cards: null,
+  archetypes: null,
   fetchCard(url) {
     axios.get(url)
     .then(response => {
       this.cards = response.data.data
       this.loading = false
+    })
+    .catch(err => {
+      console.error(err.message)
     })
   }
 })
